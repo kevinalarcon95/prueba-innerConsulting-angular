7fe3e9a7ef66c7d46d622aee80fd5c4a03001c79
Kevin Alarcón <57530393+kevinalarcon95@users.noreply.github.com>
2025-04-30 16:24:09 -0500

    feat(app): enable standalone component and add HTTP client provider

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
-import { AuthGuard } from './auth/auth.guard';
-import { LoginComponent } from './auth/login/login.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './auth/login/login.component';
+import { CharterDetailComponent } from './home/charter-detail/charter-detail.component';
+import { AuthGuard } from './auth/auth.guard';
 
 export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: 'character/:id', component: CharterDetailComponent, canActivate: [AuthGuard] },
   { path: '**', redirectTo: 'login' }
 ];
